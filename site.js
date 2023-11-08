0d600552fd65373a37d1a85872fffa4f2391a7c8
bgarth <45801518+bgarth@users.noreply.github.com>
2023-11-07 21:24:59 -0600

    Added comments to the javascript code

--- site.js
+++ site.js
@@ -1,10 +1,15 @@
+// Get the form element
 const form = document.querySelector("#physical-form")
+
+// Event listener wait for the form to be submitted
 form.addEventListener("submit", (e) => {
+    // Get each element
     const first_name = document.querySelector("#first-name").value;
     const last_name = document.querySelector("#last-name").value;
     const height = document.querySelector("#height").value;
     const weight = document.querySelector("#weight").value;
 
+    // Validates the entries
     if (first_name == null || first_name == "") {
         e.preventDefault();
         console.log("You must enter a first name");
@@ -13,20 +18,14 @@ form.addEventListener("submit", (e) => {
         e.preventDefault();
         console.log("You must enter a last name");
     }
-    /*if (height == null || height == "") {
-        e.preventDefault();
-        console.log("You must enter a height");
-    }
-    if (weight == null || weight == "") {
-        e.preventDefault();
-        console.log("You must enter a weight");
-    }*/
 
+    // This prevents the page from reloading. This wouldn't normally be used, but
+    // for the purposes of this assignment, it allows the json data to be displayed on the same page.
     e.preventDefault();
 
+    // Create string of json data and display it
     const info = {firstName:first_name, lastName:last_name, "height":height, "weight":weight};
     let jsonData = JSON.stringify(info);
-    //localStorage.setItem("jsonInfo", jsonData)
 
     const outside_div = document.getElementById("outside-form-div");
     const p = document.createElement("p");
